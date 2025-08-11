2227b7acbdf89a502c9b08e56335038fcebac712
Sidharth sangelia <sidharthsangelia@gmail.com>
2025-08-11 22:21:55 +0530

    add output static in astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -8,6 +8,7 @@ import tailwindcss from "@tailwindcss/vite";
 // https://astro.build/config
 export default defineConfig({
   site: "https://sidharth-notebook.vercel.app",
+  output: 'static',
   integrations: [preact(),sitemap()],
 
   vite: {
